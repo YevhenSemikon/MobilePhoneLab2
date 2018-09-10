6dc69a48c5507ccd0d354866f12b5fe4faf11f41
Yevhen Semikon <YNSN@simcorp.com>
2018-09-10 13:49:09 +0300

    Lab number 2. Task 1-2.

---
+++ MobilePhone/MobilePhoneLibrary/MobilePhone.cs
@@ -0,0 +1,56 @@
+﻿using MobilePhone.MobileComponents.Battery;
+using MobilePhone.MobileComponents.Speaker;
+using MobilePhone.MobileComponents.Microphone;
+using MobilePhone.MobileComponents.Screen;
+using MobilePhone.MobileComponents.SimCard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobilePhone.MobileComponents.AudioJack;
+
+namespace MobilePhone {
+    public abstract class MobilePhone {
+        private IPlayback vplaybackComponent;
+
+        public IPlayback PlaybackComponent {
+            get { return vplaybackComponent; }
+            set {
+                if (value.ToString() == "UnknownHeadset") {
+                    Console.WriteLine("Playback could not be set to Mobile");
+                    vplaybackComponent = value;
+                }
+                else {
+                    Console.WriteLine("Set playback to Mobile...");
+                }
+                              
+            }
+        }
+
+        public abstract ScreenBase Screen { get; }
+        public abstract MicrophoneBase Microphone { get; }
+        public abstract BatteryBase Battery { get; }
+        public abstract SpeakerBase Speaker { get; }
+        public abstract SimCardBase SimCard { get; }
+        private void Show(IScreenImage screenImage) { Screen.Show(screenImage); }
+        public string GetDescription()
+        {
+            var descriptionBuilder = new StringBuilder();
+            descriptionBuilder.AppendLine($"Screen Type: {Screen.ToString()}");
+            descriptionBuilder.AppendLine($"Microphone Type: {Microphone.ToString()}");
+            descriptionBuilder.AppendLine($"Battery Type: {Battery.ToString()}");
+            descriptionBuilder.AppendLine($"Speaker Type: {Speaker.ToString()}");
+            descriptionBuilder.AppendLine($"SimCard Type: {SimCard.ToString()}");
+            return descriptionBuilder.ToString();
+        }
+        public void Play(object data)
+        {
+            if (data.ToString() != "UnknownHeadset") {
+                Console.WriteLine("Play sound in Mobile:");
+                PlaybackComponent.Play(data);
+            }
+
+        }
+    }
+}
---
+++ MobilePhone/MobilePhoneLibrary/SimCorpMobilePhone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobilePhone.MobileComponents.Microphone;
+using MobilePhone.MobileComponents.Screen;
+using MobilePhone.MobileComponents.Battery;
+using MobilePhone.MobileComponents.Speaker;
+using MobilePhone.MobileComponents.SimCard;
+
+namespace MobilePhone {
+    public class SimCorpMobilePhone : MobilePhone {
+        public override ScreenBase Screen { get { return vOLEDScreen; } }
+        public override MicrophoneBase Microphone { get { return vDynamicMicrophone; } }
+        public override BatteryBase Battery { get { return vLiPolBattery; } }
+        public override SpeakerBase Speaker { get { return vStereoSpeaker; } }
+        public override SimCardBase SimCard { get { return vSimCard; } }
+        private readonly OLEDScreen vOLEDScreen = new OLEDScreen();
+        private readonly DynamicMicrophone vDynamicMicrophone = new DynamicMicrophone();
+        private readonly LiPolBattery vLiPolBattery = new LiPolBattery();
+        private readonly StereoSpeaker vStereoSpeaker = new StereoSpeaker(30);
+        private readonly LifeSimCard vSimCard = new LifeSimCard ();
+    }
+}
